5151813b30e18a17a06e3149ac18f6b2c3c0dc51
Durgesh Rao <drao@etouch.net>
2017-09-14 16:18:42 +0530

    Updated for test method description.

--- Conns_POC/src/main/java/com/etouch/connsTests/Conns_Credit_App_Page.java
+++ Conns_POC/src/main/java/com/etouch/connsTests/Conns_Credit_App_Page.java
@@ -778,7 +778,7 @@ public class Conns_Credit_App_Page extends BaseTest {
 		}
 	}
 
-	@Test(priority = 1028, enabled = true, description = "verify Successful Submit With Main source of Income as Retired And App_Status as Declined")
+	@Test(priority = 1028, enabled = true, description = "verify_Credit_App_Submit_With_Statue_Declined")
 	public void verify_Credit_App_Submit_With_Main_Source_Of_Income_As_Retired_Statue_Declined() throws Exception {
 		SoftAssert softAssert = new SoftAssert();
 		try {
@@ -978,7 +978,7 @@ public class Conns_Credit_App_Page extends BaseTest {
 		}
 	}
 
-	@Test(priority = 1031, enabled = true, description = "verify_YesLease_Submit_With_Unique_ID")
+	@Test(priority = 1031, enabled = true, description = "verify_YesLease_Submit_With_Valid_Data")
 	public void verify_YesLease_Submit_With_Valid_Data() throws Exception {
 		SoftAssert softAssert = new SoftAssert();
 		try {
