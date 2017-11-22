304584963695c41db4dc97f0267ec5dcd9cf9e3a
asingh <asingh@192.168.7.106>
2017-11-22 15:32:06 +0530

    Code Refactor Modified Else condition

--- Conns_POC/src/main/java/com/etouch/connsTests/Conns_Store_Locator_Page.java
+++ Conns_POC/src/main/java/com/etouch/connsTests/Conns_Store_Locator_Page.java
@@ -530,7 +530,7 @@ public class Conns_Store_Locator_Page extends BaseTest {
 				softAssert.assertTrue(actualCssValues.get(0).contains(verifyViewAllLinkData[1][1]), "CSS value verification failed for font size " + verifyViewAllLinkData[0][1] + "Expected Value : "+ verifyViewAllLinkData[1][1] + " Actual Value : " + actualCssValues.get(0));
 			}
 			
-			else if( browserName.equalsIgnoreCase("edge")){
+			if( browserName.equalsIgnoreCase("edge")){
 				log.info("Test Type Name Font Size Verification Edge 2 : " + testType.toString() + "****************^^^^^^^^^^^^^^******************" +  browserName.toString());
 				softAssert.assertTrue(actualCssValues.get(0).contains(verifyViewAllLinkData[6][1]), "CSS value verification failed for font size " + verifyViewAllLinkData[0][1] + "Expected Value : "+ verifyViewAllLinkData[6][1] + " Actual Value : " + actualCssValues.get(0));
 			
@@ -561,7 +561,7 @@ public class Conns_Store_Locator_Page extends BaseTest {
 				softAssert.assertTrue(actualCssValues.get(2).toLowerCase().replaceAll("'", "").replaceAll("\"", "").contains(verifyViewAllLinkData[3][1]),"CSS value verification failed for link " + verifyViewAllLinkData[0][1] + "Expected Value : "+ verifyViewAllLinkData[3][1] + " Actual Value : " + actualCssValues.get(2));				
 			}
 			
-			else if( browserName.equalsIgnoreCase("edge")){
+			 if( browserName.equalsIgnoreCase("edge")){
 				log.info("Test Type Name CSS value verification failed for link Edge b  : " + testType.toString() + "****************^^^^^^^^^^^^^^******************" +  browserName.toString());
 				softAssert.assertTrue(actualCssValues.get(2).toLowerCase().replaceAll("'", "").replaceAll("\"", "").contains(verifyViewAllLinkData[3][1]),"CSS value verification failed for link " + verifyViewAllLinkData[0][1] + "Expected Value : "+ verifyViewAllLinkData[3][1] + " Actual Value : " + actualCssValues.get(2));				
 				
