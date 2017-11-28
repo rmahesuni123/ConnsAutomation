6bf79ad8bb9bf2ffa5fa3ad5beeff6a177abb41f
asingh <asingh@192.168.7.106>
2017-11-28 10:14:53 +0530

    making changes for if condition,using Safari as a testBedName for Safari Execution Verify_LinksRedirection_Under_Appliances_Menu Conns_Home_Page

--- Conns_POC/src/main/java/com/etouch/connsTests/Conns_Home_Page.java
+++ Conns_POC/src/main/java/com/etouch/connsTests/Conns_Home_Page.java
@@ -571,17 +571,17 @@ public class Conns_Home_Page extends BaseTest {
 			String elementXpath=null;
 			Thread.sleep(2000);
 
-			if (testType.equalsIgnoreCase("Web")) {
+			//if (testType.equalsIgnoreCase("Web")) {
 			//  if (testType.equalsIgnoreCase("Web") && (!(browserName.equalsIgnoreCase("Edge") ))) {
-			// if (testType.equalsIgnoreCase("Web") && (!(testBedName.equalsIgnoreCase("Edge") ))) {
+			 if (testType.equalsIgnoreCase("Web") && (!(testBedName.equalsIgnoreCase("Safari") ))) {
 
 				  log.info("TestType is  : " +testType +"************ testBedName **************" +testBedName );
 				for (int i = 0; i < testData.length; i++) {
 					log.info("Iteration under test : " + i);
 					Thread.sleep(3000);
-					//commonMethods.hoverOnelementbyXpath(webPage, testData[0][0], softAssert);
+					commonMethods.hoverOnelementbyXpath(webPage, testData[0][0], softAssert);
 					/************************Using hoverOnelementbyXpath1 for hovering :  Verify_LinksRedirection_Under_Appliances_Menu********************/
-					commonMethods.hoverOnelementbyXpath1(webPage, testData[0][0], softAssert);
+				//	commonMethods.hoverOnelementbyXpath1(webPage, testData[0][0], softAssert);
 
 					
 					/*WebElement element_2 = webPage.getDriver().findElement(By.xpath(commonData[0][0]));					
@@ -590,9 +590,10 @@ public class Conns_Home_Page extends BaseTest {
 					commonMethods.verifyLinkStatusCodeAndHrefValue(webPage, testData[i][2], testData[i][1], testData[i][2], softAssert);
 				}
 			}
-			if (testType.equalsIgnoreCase("Mobile")) {
-		 //  if (testType.equalsIgnoreCase("Mobile") || (testType.equalsIgnoreCase("Web") && (testBedName.equalsIgnoreCase("Edge")))) {
-			   log.info("TestType is  : " +testType +"************ testBedName **************" +browserName );
+		//	if (testType.equalsIgnoreCase("Mobile")) {
+			/***********************making changes for if condition,using Safari as a testBedName for Safari Execution*************/
+		   if (testType.equalsIgnoreCase("Mobile") || (testType.equalsIgnoreCase("Web") && (testBedName.equalsIgnoreCase("Safari")))) {
+			   log.info("TestType is  : " +testType +"************ testBedName **************" +testBedName );
 			   for (int i = 0; i < testData.length; i++) {
 					log.info("Iteration under test : " + i);
 					if(!(testData[i][4].equalsIgnoreCase("NA"))){
